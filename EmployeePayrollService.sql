e4381214f3411c95ad1a7a71f7751cc0766db6c4
unknown <kinekarpallavi21@gmail.com>
2022-04-21 17:32:39 +0530

    Insert data into table employee_payroll

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -10,3 +10,13 @@ Salary bigint,
 StartDate datetime);
 --gives the info about the table named employee_payroll
 select * from employee_payroll;
+--Insert data in table employee_payroll
+insert into employee_payroll(Id,Name,Salary,StartDate)values
+(1,'Omkar',40000,'2022-04-21'),
+(2,'Anjali',25000,'2022-02-25'),
+(3,'Arjun',40000,'2022-01-17'),
+(4,'Pratik',30000,'2022-04-01'),
+(5,'Arnav',45000,'2022-07-20')
+;
+--views all data in employee_payroll
+select * from employee_payroll;

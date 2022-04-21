ff88db7891d2f2db423f216b19ee4fd8397d889f
unknown <kinekarpallavi21@gmail.com>
2022-04-21 16:35:24 +0530

    Create table named employee_payroll in payroll_service database

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -2,3 +2,11 @@
 create database payroll_service;
 --selects the payroll_service database for use
 use payroll_service;
+--creates table named employee_payroll in payroll_service database
+create table employee_payroll(
+Id int not null primary key,
+Name varchar(50),
+Salary bigint,
+StartDate datetime);
+--gives the info about the table named employee_payroll
+select * from employee_payroll;
